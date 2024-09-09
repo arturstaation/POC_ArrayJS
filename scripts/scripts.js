83790dba323ab6963617541fc948804520799cd4
arturstaation <arturmackenzie12@gmail.com>
2024-09-09 20:03:56 -0300

    padronizando

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -97,6 +97,6 @@ function changeInputMap() {
 }
 
 function adjustTextareaHeight(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = (textarea.scrollHeight) + 'px';
+    textarea.style.height = "auto";
+    textarea.style.height = (textarea.scrollHeight) + "px";
 }
